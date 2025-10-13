7dce59cc3a3896bf5817611dc4c69179eb23be9f
w1esther <w1mariaesther@gmail.com>
2025-10-13 08:57:17 -0300

    atualizações js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,14 +1,34 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const icone = document.getElementById("coroa1");
-  if (localStorage.getItem("outraVisitada") === "true") {
+  const fases = [{ id: "coroa1", key: "fase1Concluida" },
+    { id: "coroa2", key: "fase2Concluida" },
+    { id: "coroa3", key: "fase3Concluida" }];
+  
+  if (localStorage.getItem(fases.key) === "true") {
+    fases.forEach(id => {
+      const icone = document.getElementById(fases.id);
     if (icone) icone.classList.add("visitado");
+    });
   }
 
   // Marcar como visitado ao clicar no botão "CONCLUIR FASE"
-  const concluirBtn = document.getElementById("concluirf1");
-  if (concluirBtn) {
+  const concluirF1 = document.getElementById("concluirf1");
+  if (concluirF1) {
     concluirBtn.addEventListener("click", () => {
       localStorage.setItem("outraVisitada", "true");
     });
   }
 });
+
+const concluirF2 = document.getElementById("concluirf2");
+if (concluirF2) {
+  concluirBtn.addEventListener("click", () => {
+    localStorage.setItem("fase2concluida", "true");
+    });
+  }
+
+const concluirF3 = document.getElementById("concluirf3");
+if (concluirF3) {
+  concluirBtn.addEventListener("click", () => {
+    localStorage.setItem("fase3concluida", "true");
+    });
+  }
